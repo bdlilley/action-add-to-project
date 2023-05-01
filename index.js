5e648e28ec033462f09c6213f226dbd19a62cf0b
bensolo-io <benji.lilley@solo.io>
2023-05-01 13:03:29 -0400

    wip

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Octokit } = require("@octokit/rest");
 
 try {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+  });
   const projectId = core.getInput('project');
   const issueId = core.getInput('issue');
   console.log(`add ${issueId} to ${projectId}`);
 
+  octokit.projects
+    .listForOrg({org: "solo-io", type: "private"})
+    .then(({ data }) => {
+      console.log(JSON.stringify(data))
+    });
+
 //   const time = (new Date()).toTimeString();
 //   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
